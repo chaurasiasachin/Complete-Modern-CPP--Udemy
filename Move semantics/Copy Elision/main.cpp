202d185c6605e6232b415da46e97380cbc7a4118
= <cse88888@gmail.com>
2026-02-28 12:49:58 +0530

    NRVO Copy Elision

--- Move semantics/Copy Elision/main.cpp
+++ Move semantics/Copy Elision/main.cpp
@@ -2,10 +2,31 @@
 #include <iostream>
 
 Integer Add (int a, int b){
-   Integer temp(a+b);
-   return temp;
+   Integer temp(a+b); // 1 object is created
+   return temp; // this object is return by value. So, another object is created.
 }
 int main(){
-    Integer a = 3;
+    //Integer a = 3; // Integer a = Integer(3);
+    Integer a = Add(3,5); // the object created at line 6 is copied and moved into a.
     return 0;
 }
+// NRVO (Named return value optimisation)
+/*
+g++ -std=c++17 main.cpp Integer.cpp -o main && ./main
+Integer(int )
+~Integer()
+
+g++ -std=c++17 -fno-elide-constructors main.cpp Integer.cpp -o main && ./main
+Integer(int )
+Integer(Integer &&)
+~Integer()
+~Integer()
+
+g++ -std=c++17 main.cpp Integer.cpp -o main && ./main
+Integer(int )
+~Integer()
+
+g++ -std=c++14 main.cpp Integer.cpp -o main && ./main
+Integer(int )
+~Integer()
+*/
