fac38a29b8a5cfce4bde0e33a754b3e3d4f10a7f
= <cse88888@gmail.com>
2026-03-01 11:30:01 +0530

    RVO

--- Move semantics/Copy Elision/main.cpp
+++ Move semantics/Copy Elision/main.cpp
@@ -2,8 +2,20 @@
 #include <iostream>
 
 Integer Add (int a, int b){
+    /*
+    NVRO
    Integer temp(a+b); // 1 object is created
    return temp; // this object is return by value. So, another object is created.
+                // NRVO
+                 // Here the compiler has not created a copy of temp object            
+                //becaise its elided. This is another form of copy elision and is
+                    // Named return value optimization (NRVO)
+   */
+    return Integer(a+b); // this is better creating temp object in return itself.
+                    //even here compiler will elide the copy that needs to be created 
+                    // when we return object from here. This is called as
+                    // (RVO) Return value optimization 
+
 }
 int main(){
     //Integer a = 3; // Integer a = Integer(3);
